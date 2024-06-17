17831236372e8509e1335870c0b86d259fae487d
noscopeFanEX <starnesx@gmail.com>
2024-06-17 12:54:50 -0400

    code42

--- concatenation.js
+++ concatenation.js
@@ -9,11 +9,12 @@ submitButton.addEventListener("click", function() {
     let birthYear = document.querySelector(".year").value;
     let age = 2023 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let city = document.querySelector(".city").value; 
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
+    results.innerHTML = ("My name is " + myName + ". Im feeling " + mood + ". I like to eat "+ food + ". I was born in " + birthYear + ". My age is " + age + ". Im from " + city);
 
 
 });
--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let buttonOne = document.querySelector(".button-one");
 let captionOne = document.querySelector(".caption-one");
-
+let buttonTwo = document.querySelector(".button-two");
+let captionTwo = document.querySelector(".caption-two");
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
+ let spidyCaption = document.querySelector(".comment-one").value;
 
 
     // 2. Display the variable spidyCaption to captionOne.
-
+    captionOne.innerHTML = (spidyCaption);
 
 });
 
 // 3. Repeat the previous steps for the second meme.
 //  - Be sure to store variables for the button and caption.
+buttonTwo.addEventListener("click",function(){
+ let kermit = document.querySelector(".comment-two").value;
+ captionTwo.innerHTML = (kermit);
+
+
+
+
+}); 
